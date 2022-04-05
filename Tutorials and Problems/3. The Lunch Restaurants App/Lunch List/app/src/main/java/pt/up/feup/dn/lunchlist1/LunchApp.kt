cdfeb6898cf0c71eabe9228b33b69141ef5e86d8
Diogo Nunes <up201808546@fe.up.pt>
2022-04-05 17:41:48 +0100

    lunch restaurants app done

--- Tutorials and Problems/3. The Lunch Restaurants App/Lunch List/app/src/main/java/pt/up/feup/dn/lunchlist1/LunchApp.kt
+++
@@ -1,9 +0,0 @@
-package pt.up.feup.dn.lunchlist1
-
-import android.app.Application
-
-class LunchApp: Application() {
-    val rests: ArrayList<Restaurant> = ArrayList()
-    var current: Restaurant? = null
-    var adapter: RestaurantAdapter? = null
-}
