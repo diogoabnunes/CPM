83de5f63f34104379a8edc422310eb41146476a4
marinuas <marinadias1500@hotmail.com>
2022-04-23 17:33:52 +0100

    started login page

---
+++ Assignments/Proj1/app/src/main/java/com/example/proj1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.proj1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //val email:EditText = findViewById(R.id.email)
+    //val password:EditText = findViewById(R.id.password)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //email.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.vector_mail, 0, 0, 0)
+        //password.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.vector_locker, 0, 0, 0)
+
+    }
+}
