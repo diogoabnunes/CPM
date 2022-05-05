078a8b1638146d4ca7bd5cd7863631bdd6f784eb
jessymireie <up201806723@up.pt>
2022-05-05 17:23:26 +0100

    code for scan added, needs testing in device

--- Assignments/Acme-Electronics-Shop/PrinterTerminal/app/src/main/java/pt/up/feup/cpm/printerterminal/MainActivity.kt
+++ Assignments/Acme-Electronics-Shop/PrinterTerminal/app/src/main/java/pt/up/feup/cpm/printerterminal/MainActivity.kt
@@ -1,11 +1,113 @@
 package pt.up.feup.cpm.printerterminal
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import com.budiyev.android.codescanner.*
+
+private const val CAMERA_REQUEST_CODE=101
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var codeScanner: CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupPermissions()
+        //codeScanner()
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(this, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+
+
     }
+    /*private fun codeScanner(){
+        codeScanner=CodeScanner(this, scanner_view)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats= CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode= ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+
+            decodeCallback= DecodeCallback {
+                runOnUiThread{
+                    text_view.text=it.text
+                }
+
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread{
+                    Log.e("Main", "Camera initiation erro: ${it.message}")
+                }
+            }
+        }
+    }*/
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        if(permission!= PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+    }
+
+   /* override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"You need the camera permission to be able to use the scanner!",Toast.LENGTH_LONG)
+                }
+            }
+        }
+    }*/
 }
