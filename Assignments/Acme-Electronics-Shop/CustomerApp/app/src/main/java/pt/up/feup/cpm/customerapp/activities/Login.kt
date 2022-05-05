f829f579e012aeecd12db63ac80fde4c78667034
marinuas <marinadias1500@hotmail.com>
2022-05-05 16:22:03 +0100

    trying to login with password

--- Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/Login.kt
+++ Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/Login.kt
@@ -3,16 +3,22 @@ package pt.up.feup.cpm.customerapp.activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import pt.up.feup.cpm.customerapp.R
 
 class Login : AppCompatActivity() {
     val tvResponse by lazy { findViewById<TextView>(R.id.tv_response) }
+    val email by lazy { findViewById<EditText>(R.id.email) }
+    val password by lazy { findViewById<EditText>(R.id.password) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        login()
         setupHyperlink()
     }
 
@@ -30,5 +36,20 @@ class Login : AppCompatActivity() {
 
     fun writeText(value: String) {
         runOnUiThread { tvResponse.text = value }
+    }
+
+    fun login(){
+        val user = email.text.toString()
+        val pass = password.text.toString()
+        val login = findViewById<Button>(R.id.login_button)
+        login.setOnClickListener {
+            if(user.equals("cpm@feup.pt") && pass.equals("feup")) {
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "nhe", Toast.LENGTH_LONG)
+            }
+        }
     }
 }
