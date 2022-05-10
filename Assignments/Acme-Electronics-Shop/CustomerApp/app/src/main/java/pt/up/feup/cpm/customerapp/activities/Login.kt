71c4e2d18bcd52eb139e76f37c8970f637b84b11
marinuas <marinadias1500@hotmail.com>
2022-05-10 10:12:28 +0100

    login working

--- Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/Login.kt
+++ Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/Login.kt
@@ -3,22 +3,25 @@ package pt.up.feup.cpm.customerapp.activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import pt.up.feup.cpm.customerapp.R
 
 class Login : AppCompatActivity() {
     val tvResponse by lazy { findViewById<TextView>(R.id.tv_response) }
+    /*val loginbtn = findViewById<Button>(R.id.login_button)
     val email by lazy { findViewById<EditText>(R.id.email) }
-    val password by lazy { findViewById<EditText>(R.id.password) }
+    val password by lazy { findViewById<EditText>(R.id.password) }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        login()
+        //login()
         setupHyperlink()
     }
 
@@ -38,18 +41,31 @@ class Login : AppCompatActivity() {
         runOnUiThread { tvResponse.text = value }
     }
 
-    fun login(){
-        val user = email.text.toString()
-        val pass = password.text.toString()
-        val login = findViewById<Button>(R.id.login_button)
-        login.setOnClickListener {
-            if(user.equals("cpm@feup.pt") && pass.equals("feup")) {
+    /*fun login(){
+        loginbtn.setOnClickListener(){
+            if(email.text.toString().equals("cpm@feup.pt") && password.text.equals("feup")) {
                 val intent = Intent(this, Home::class.java)
                 startActivity(intent)
             }
             else{
-                Toast.makeText(this, "nhe", Toast.LENGTH_LONG)
+                Toast.makeText(this, "LOGIN FAILED!!", Toast.LENGTH_SHORT).show()
             }
         }
+    }*/
+    fun onclick(view: View) {
+        login(
+            findViewById<EditText>(R.id.email).text.toString(),
+            findViewById<EditText>(R.id.password).text.toString()
+        )
     }
+    fun login(userName: String, password: String) {
+        if (userName == "cpm@feup.pt" && password == "1234") {
+            startActivity(Intent(this, Home::class.java))
+            Toast.makeText(this, "Login Success!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
