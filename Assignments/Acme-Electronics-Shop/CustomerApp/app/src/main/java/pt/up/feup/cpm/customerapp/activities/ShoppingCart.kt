bc8d4d66005e8aed196b5493d3c2a7a3f8528216
marinuas <marinadias1500@hotmail.com>
2022-05-12 15:02:10 +0100

    botoes nos items

--- Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/ShoppingCart.kt
+++ Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/ShoppingCart.kt
@@ -2,11 +2,17 @@ package pt.up.feup.cpm.customerapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import pt.up.feup.cpm.customerapp.R
 
 class ShoppingCart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_cart)
+
+        var productName = findViewById<TextView>(R.id.productName)
+        var productPrice = findViewById<TextView>(R.id.price)
+        var productQuant = findViewById<TextView>(R.id.quantity)
+
     }
 }
