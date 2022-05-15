a8daa9a2d6fdc20cffe60383a8a78db13c773d8c
marinuas <marinadias1500@hotmail.com>
2022-05-16 00:47:11 +0100

    algumas alteraçoes

--- Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/ShoppingCart.kt
+++ Assignments/Acme-Electronics-Shop/CustomerApp/app/src/main/java/pt/up/feup/cpm/customerapp/activities/ShoppingCart.kt
@@ -2,14 +2,13 @@ package pt.up.feup.cpm.customerapp.activities
 
 import android.app.Activity
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.size
 import com.google.gson.Gson
 import com.google.zxing.integration.android.IntentIntegrator
 import org.json.JSONObject
@@ -31,6 +30,9 @@ class ShoppingCart : AppCompatActivity() {
             transaction = result.data?.getSerializableExtra("value") as Transaction
         }
     }
+    private lateinit var quantity: TextView
+    private lateinit var increase_btn: ImageButton
+    private lateinit var decrease_btn: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +40,9 @@ class ShoppingCart : AppCompatActivity() {
 
         setupPayButton()
         setupAddItemButton()
+        //increaseQuantity()
+        //decreaseQuantity()
+
 
         var listview = findViewById<ListView>(R.id.list_item)
         var list = mutableListOf<TransactionItem>()
@@ -108,4 +113,29 @@ class ShoppingCart : AppCompatActivity() {
     fun writeText(value: String) {
         runOnUiThread { transaction_res.text = value }
     }
+/*
+    private fun increaseQuantity(){
+        quantity = findViewById(R.id.quantity)
+        increase_btn = findViewById(R.id.increase_btn)
+
+        var quant = 0
+
+        increase_btn.setOnClickListener{
+            quant += 1
+
+            quantity.text = quant.toString()
+        }
+    }
+    private fun decreaseQuantity(){
+        quantity = findViewById(R.id.quantity)
+        decrease_btn = findViewById(R.id.decrease_btn)
+
+        var quant = 0
+
+        decrease_btn.setOnClickListener{
+            quant -= 1
+
+            quantity.text = quant.toString()
+        }
+    }*/
 }
