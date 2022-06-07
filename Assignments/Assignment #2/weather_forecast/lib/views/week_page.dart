ca2d942180cc695336f2138d06f2f43f835ac154
marinuas <marinadias1500@hotmail.com>
2022-06-07 12:18:53 +0100

    weekly page almost done

--- Assignments/Assignment #2/weather_forecast/lib/views/week_page.dart
+++ Assignments/Assignment #2/weather_forecast/lib/views/week_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_forecast/models/weather_info.dart';
 import 'package:weather_forecast/models/forecast_info.dart';
+import 'package:intl/intl.dart';
 
 class WeekPage extends StatefulWidget {
   const WeekPage(
@@ -22,21 +23,103 @@ class _WeekPageState extends State<WeekPage> {
       resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
           child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/degrade.jpg'),
-                  fit: BoxFit.cover),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/degrade.jpg'),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 20,),
+            Row(children: const [
+              const SizedBox(width: 70,),
+              Text(
+                  'This Week Forecast',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            ]),
+            const SizedBox(height: 40),
+            SizedBox(
+              height: 600,
+              child: ListView.builder(
+                itemCount: 5,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) => SizedBox(
+                  child: Column(children: [
+                    Row(children: [
+                      row(
+                        DateFormat('EEEE').format(DateTime.parse('${widget.forecastInfo.list?[index].dtTxt}')),
+                        'https://openweathermap.org/img/wn/${widget.forecastInfo.list?[index].weatherIcon}@4x.png',
+                      (widget.forecastInfo.list?[index].mainTempMax?.toInt()).toString(),
+                      (widget.forecastInfo.list?[index].mainTempMin?.toInt()).toString(),
+                      )
+                    ]),
+                  ]),
+                ),
+              ),
             ),
-            child: Column(
-              children: [
-                Row(children: const [
-                  Text('Temperature Tomorrow (ºC)')
-                ]),
-                const SizedBox(height: 5),
-              ],
-            ),
-          )
-      ),
+          ],
+        ),
+      )),
     );
+  }
+
+  Row row(day, icon, maxTemp, minTemp) {
+    return Row(children: <Widget>[
+      const SizedBox(width: 10),
+      Align(
+        alignment: Alignment.centerLeft,
+        child: SizedBox(
+          width: 150,
+          height: 50,
+          child: Text(day,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              )),
+        ),
+      ),
+      Align(
+        alignment: Alignment.centerLeft,
+        child: SizedBox(
+          width: 80,
+          height: 50,
+          child: Image.network(
+            icon,
+            width: 40,
+          ),
+        ),
+      ),
+      const SizedBox(width: 60),
+      Row(children: <Widget>[
+        Align(
+          alignment: Alignment.centerLeft,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: Text(
+              maxTemp + "º  " ,
+              style: const TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: Text(
+              minTemp + "º",
+              style: const TextStyle(color: Colors.black45, fontSize: 25),
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+      ]),
+    ]);
   }
 }
